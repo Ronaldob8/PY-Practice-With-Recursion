c29589a0f78126dfb1b73ae3294f74f8f0bc159f
Ronaldob8 <97202631+Ronaldob8@users.noreply.github.com>
2022-11-28 20:03:10 -0800

    done

--- solution3.py
+++ solution3.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 3 below
+def fibbonacci(n):
+    if n == 1:
+        return 0
+
+    if n <= 2:
+        return 1
+
+    else:
+        return fibbonacci(n-1)+fibbonacci(n-2)
+
+
+print(fibbonacci(4))
